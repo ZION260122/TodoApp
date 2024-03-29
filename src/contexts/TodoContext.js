e67cab1fc210ed7f6f200e3a00bf55dab6026315
utkarsh <98471140+ZION260122@users.noreply.github.com>
2024-03-30 00:35:30 +0530

    Add files via upload

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,23 @@
+
+import {createContext, useContext} from "react"
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: " Todo msg",
+            completed: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+})
+
+//useTodo is a custom Hook and usecontext is also a hook form react
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
